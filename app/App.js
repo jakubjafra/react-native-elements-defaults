efff02c46c2df7119f95684fb5291ec568c7ab4f
Jakub Jafra <jakub.jafra@gmail.com>
2017-12-20 22:56:11 +0100

    lib

--- app/App.js
+++ app/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-elements-styled-lib';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Button title='Open up App.js to start working on your app!' />
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
